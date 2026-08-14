eb86c7b511260610abd3f9d805a2a0946c4c4b7a
M1rt3n <martinperezlarra@gmail.com>
2026-08-13 22:00:26 -0300

    Add files via upload

---
+++ Verificación de alumnos - Incompleto -/Verificación de alumnos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Verificación_de_alumnos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
