f8c7f5254790e694e32c369bde5015e6d0f8be1e
M1rt3n <martinperezlarra@gmail.com>
2026-08-14 22:10:55 -0300

    Add files via upload

---
+++ Verificación_de_alumnos/Verificación Alumnos.Presentación/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Verificación_Alumnos.Negocios;
+
+namespace Verificación_Alumnos.Presentación
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Ingrese Legajo: ");
+            string Legajo = Console.ReadLine();
+
+            AlumnoNegocio negocio = new AlumnoNegocio();
+
+            Alumno alumno = negocio.ObtenerAlumno(Legajo);
+
+            if (alumno != null)
+                Console.WriteLine($"Encontrado: {alumno.Nombre} , Condición: ${alumno.Condición}");
+            else
+                Console.WriteLine("No existe.");
+        }
+
+    }
+}
