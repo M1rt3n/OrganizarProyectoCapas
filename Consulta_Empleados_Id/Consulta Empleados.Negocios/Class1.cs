733dc0d20a600ca9119e49706724e6a957ae4879
M1rt3n <martinperezlarra@gmail.com>
2026-08-14 22:12:56 -0300

    Add files via upload

---
+++ Consulta_Empleados_Id/Consulta Empleados.Negocios/Class1.cs
@@ -0,0 +1,30 @@
+﻿using Consulta_Empleados.Datos;
+namespace Consulta_Empleados.Negocios
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Puesto { get; set; }
+        public string Departamento { get; set; }
+    }
+    public class EmpleadosNegocio
+    {
+        private EmpleadosDatos _datos = new EmpleadosDatos();
+
+        public Empleado ObtenerEmpleado(int Id)
+        {
+            var resultado = _datos.BuscarPorId(Id);
+
+            if (resultado == null) return null;
+
+            return new Empleado
+            {
+                Id = resultado.Value.Id,
+                Nombre = resultado.Value.Nombre,
+                Puesto = resultado.Value.Puesto,
+                Departamento = resultado.Value.Departamento
+            };
+        }
+    }
+}
---
+++ Consulta_Empleados_Id/Consulta Empleados.Presentación/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Consulta_Empleados.Negocios;
+namespace Consulta_Empleados.Presentación
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Ingrese Id: ");
+            int Id = Convert.ToInt32(Console.ReadLine());
+
+            EmpleadosNegocio negocio = new EmpleadosNegocio();
+
+            Empleado empleado = negocio.ObtenerEmpleado(Id);
+
+            if (empleado != null)
+                Console.WriteLine($"Empleado encontrado: {empleado.Nombre}, Puesto: ${empleado.Puesto}, Departamento: ${empleado.Departamento}");
+            else
+                Console.WriteLine("Empleado no encontrado");
+        }
+    }
+}
