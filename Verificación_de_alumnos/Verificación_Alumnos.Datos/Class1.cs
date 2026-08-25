1a8429d8f91e3bcd1a3758dadb2394321ddc8629
M1rt3n <martinperezlarra@gmail.com>
2026-08-25 05:46:53 -0300

    Add files via upload

--- Verificación_de_alumnos/Verificación_Alumnos.Datos/Class1.cs
+++ Verificación_de_alumnos/Verificación_Alumnos.Datos/Class1.cs
@@ -1,21 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 using System.Linq;
 namespace Verificación_Alumnos.Datos
 {
     public class AlumnoDatos
     {
-        private List<(string Legajo, string Nombre, int Condición)> _tablaAlumnos = new List<(string, string, int)>
-        {
-            ("981318", "Gilbert", 10),
-            ("410947", "Analia", 9)
-        };
+        private string _conexionString =
+     "Server=127.0.0.1;Port=3306;Database=archivo;Uid=root;Pwd=;";
 
         public (string Legajo, string Nombre, int Condición)? BuscarPorLegajo(string legajo)
         {
-            var resultado = _tablaAlumnos.FirstOrDefault(p => p.Legajo == legajo);
-            if (resultado.Legajo == null) return null;
-            return resultado;
-        }
+            string query = "SELECT Legajo, Nombre, Condición FROM alumno WHERE Legajo = @Legajo";
+            using (MySqlConnection conexion = new MySqlConnection(_conexionString))
+            {
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+                comando.Parameters.AddWithValue("@Legajo", legajo);
+                conexion.Open();
+                using (MySqlDataReader reader = comando.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        string legajoDb = reader["Legajo"].ToString();
+                        string nombreDb = reader["Nombre"].ToString();
+                        int condiciónDb = reader.GetInt32("Legajo");
 
+                        return (legajoDb, nombreDb, condiciónDb);
+                    }
+                }
+            }
+            return null;
+        }
     }
 }
+
